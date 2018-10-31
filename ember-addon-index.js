48a52a4d5afedcf2f8a3fbee2ccda2cc197dbcaa
Jonathan Jackson <jonathan.jackson1@me.com>
2018-10-31 15:28:38 -0400

    Install helpers directly into srcTree via preprocessTree hook

--- ember-addon-index.js
+++ ember-addon-index.js
@@ -1,8 +1,27 @@
+const Funnel = require('broccoli-funnel');
+const BroccoliDebug = require('broccoli-debug');
 const path = require('path');
+const MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'glimmer-truth-helpers',
   blueprintsPath() {
-    return path.join(__dirname, 'blueprints');
+    // disable blueprints for now
+    return null;
+  },
+  preprocessTree(type, tree) {
+    if (type === 'src') {
+      let debugTree = BroccoliDebug.buildDebugCallback(`glimmer-truth-helpers`);
+
+      let helperTree = debugTree(new Funnel(path.join(__dirname, 'blueprints', 'glimmer-truth-helpers', 'files'), 'helperTree'));
+
+      tree = debugTree(tree, 'srcTree');
+
+      let outputTree = debugTree(new MergeTrees([helperTree, tree]), 'outputTree')
+
+      return outputTree;
+    } else {
+      return tree;
+    }
   }
 }
