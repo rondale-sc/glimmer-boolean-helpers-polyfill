1e2e5232d9919b880188054896ff349b489ff352
Jonathan Jackson <jonathan.jackson1@me.com>
2018-10-31 13:59:17 -0400

    Add `or` helper

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ export { default as gte } from '.src/helpers/gte';
 export { default as lt } from '.src/helpers/lt';
 export { default as lte } from '.src/helpers/lte';
 export { default as not } from '.src/helpers/not';
+export { default as or } from '.src/helpers/or';
---
+++ src/helpers/or.js
@@ -0,0 +1,12 @@
+import truthConvert from '../utils/truth-convert';
+
+export function or(params) {
+  for (let i=0, len=params.length; i<len; i++) {
+    if (truthConvert(params[i]) === true) {
+      return params[i];
+    }
+  }
+  return params[params.length-1];
+}
+
+export default or;
